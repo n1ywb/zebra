5b15f5b325412970d7d80bf16123aba96a725e29
Jeff Laughlin <jeff@jlconsulting.(none)>
2013-01-28 11:38:01 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+zebra
+=====
+
+Fake Python API for BRTT Antelope.
+
+Zebra is not Antelope. A zebra is not an antelope.
+
---
+++ antelope/_brttpkt.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+def _orbreapthr_new2(orbname, select, reject, tafter, timeout, queuesize):
+    return object()
+
+def _orbreapthr_set_to_stop(thread):
+    return 0
+
+def _orbreapthr_is_stopped(thread):
+    return 1
+
+def _orbreapthr_get(thread):
+    # rc, pktid, srcname, pkttime, pkt, nbytes
+    return 0, 0, '', 0, '', 0
+
+def _orbreapthr_destroy(thread):
+    return 0
+
+
