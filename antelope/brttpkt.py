1d17cbdfedc9d0e0bd7e125b0e47c5cc54e45799
Jeff Laughlin <jeff@jlconsulting.(none)>
2013-05-08 21:01:05 -0400

    added simple setup.py and brttpkt.py files

---
+++ antelope/brttpkt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+class OrbreapThr(object):
+    def get(self):
+        pass
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='zebra',
+      version='0',
+      description='A zebra is not an antelope.',
+      author='Jeff Laughlin',
+      author_email='jeff@jefflaughlinconsulting.com',
+      url='https://github.com/n1ywb/zebra',
+      packages=['antelope'],
+)
+
+
